950f70e42199744647078b0d30183cee08249db5
FZTYBX <omerfurkanakckl@icloud.com>
2023-01-05 01:53:00 +0300

    SCYO_FİNAL

---
+++ scyo_elektron_proje.py
@@ -0,0 +1,14 @@
+# FİNAL PROJESİ
+
+from vpython import *
+import time
+import math
+
+#Protonlar mavi renkli,
+#nötronlar mor renkli, 
+#büyüklükleri sembolik.
+
+p1= sphere(pos=vector(0,0,0),radius = 250,text="proton1",color = color.blue)
+p2= sphere(pos=vector(500,0,0),radius = 250,text="proton1",color = color.blue)
+p3= sphere(pos=vector(250,0,250*3**0.5),radius = 250,text="proton1",color = color.blue)
+p4= sphere(pos=vector(250,0,-250*3**0.5),radius = 250,text="proton1",color = color.blue)
